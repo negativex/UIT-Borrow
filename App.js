e3da55e88d53b8c66616070bc14849f8a9e7283e
negativex <63691219+negativex@users.noreply.github.com>
2023-04-03 09:30:02 +0700

    test push 3_khong dung lenh

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>test thu push code</Text>
+      <Text>test thu push code 2</Text>
       <StatusBar style="auto" />
     </View>
   );
