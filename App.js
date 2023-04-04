f323ee4f1e936ada12ffcd4b84a351811ae77ae0
Ngọc Trần <86198119+ngoctran1401@users.noreply.github.com>
2023-04-04 15:35:02 +0700

    test

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>test thu push code 2</Text>
+      <Text>test thu push</Text>
       <StatusBar style="auto" />
     </View>
   );
