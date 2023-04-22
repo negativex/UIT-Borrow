637c7089551b2e31ec66b497507a83d4baa4b0e3
Ngọc Trần <86198119+ngoctran1401@users.noreply.github.com>
2023-04-22 16:08:34 +0700

    ui detail done

--- App.js
+++ App.js
@@ -6,33 +6,38 @@ import react from "react";
 import LoginScreen from "./layout/login";
 import RegisterScreen from "./layout/register";
 import HomeStackNavigator from "./layout/navigatorScreen";
+import Detail from "./layout/detail";
 const Stack = createStackNavigator();
 
 const App = () => {
   return (
-
-  //  LoginScreen
+    //  LoginScreen
     // <NavigationContainer>
     //   <Stack.Navigator initialRouteName='Login' headerMode="none">
     //     <Stack.Screen name="Login" component={LoginScreen} />
     //   </Stack.Navigator>
     // </NavigationContainer>
 
-      //RegisterScreen
+    //RegisterScreen
     //  <NavigationContainer>
     //   <Stack.Navigator initialRouteName='Register' headerMode="none">
     //     <Stack.Screen name="Register" component={RegisterScreen} />
     //   </Stack.Navigator>
     // </NavigationContainer>
 
-
     // NavigatorScreen
-     <NavigationContainer>
-      <Stack.Navigator initialRouteName='Navigator' headerMode="none">
-        <Stack.Screen name="Navigator" component={HomeStackNavigator} />
+    //  <NavigationContainer>
+    //   <Stack.Navigator initialRouteName='Navigator' headerMode="none">
+    //     <Stack.Screen name="Navigator" component={HomeStackNavigator} />
+    //   </Stack.Navigator>
+    // </NavigationContainer>
+
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Detail" headerMode="none">
+        <Stack.Screen name="Detail" component={Detail} />
       </Stack.Navigator>
     </NavigationContainer>
-   );
+  );
 };
 
 export default App;
