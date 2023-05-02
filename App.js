9107a671783095e4d54ee01a69858d7e000d33bd
negativex <63691219+negativex@users.noreply.github.com>
2023-05-02 18:28:14 +0700

    Update Admin Interface (Quan ly User)

--- App.js
+++ App.js
@@ -10,8 +10,11 @@ import Detail from "./layout/Screen_User/detail";
 import Profile from "./layout/Screen_User/profile";
 import LoginScreen from "./layout/Screen_User/login";
 import Admin_device_type from "./layout/Screen_Admin/device_type"
-import Admin_devices_management from "./layout/Screen_Admin/devices"
-
+import Admin_device_list from "./layout/Screen_Admin/device_list"
+import Admin_device_details from "./layout/Screen_Admin/device_details"
+import Admin_user_list from "./layout/Screen_Admin/user_list"
+import Admin_user_details from "./layout/Screen_Admin/user_details"
+import Home from "./layout/Screen_Admin/device_type";
 const Stack = createStackNavigator();
 const App = () => {
   return (
@@ -36,16 +39,9 @@ const App = () => {
     //   </Stack.Navigator>
     // </NavigationContainer>
     
-    //device_type_admin_screen
-    // <NavigationContainer>
-    // <Stack.Navigator initialRouteName='Navigator' headerMode="none">
-    //     <Stack.Screen name="Navigator" component={Admin_device_type} />
-    //   </Stack.Navigator>
-    // </NavigationContainer>
-    //device_type_admin_screen
     <NavigationContainer>
     <Stack.Navigator initialRouteName='Navigator' headerMode="none">
-        <Stack.Screen name="Navigator" component={Admin_devices_management} />
+        <Stack.Screen name="Navigator" component={Admin_user_details} />
       </Stack.Navigator>
     </NavigationContainer>
 
