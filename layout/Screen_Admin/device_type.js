b54d9fbabfdbb78550f6dd3b8c9008802bb4dccf
Ngọc Trần <86198119+ngoctran1401@users.noreply.github.com>
2023-06-16 22:24:02 +0700

    them thiet bi3

--- layout/Screen_Admin/device_type.js
+++ layout/Screen_Admin/device_type.js
@@ -1,5 +1,5 @@
 
-<<<<<<< HEAD
+
 import { View, Text, Image, Dimensions } from "react-native";
 import React from "react";
 import {
@@ -29,39 +29,6 @@ const device_type = ({ navigation }) => {
           paddingHorizontal: 20,
         }}
       >
-=======
-// import { View, Text, Image, Dimensions } from "react-native";
-// import React from "react";
-// import {
-//   TextInput,
-//   ScrollView,
-//   TouchableOpacity,
-// } from "react-native-gesture-handler";
-// import { LinearGradient } from "expo-linear-gradient";
-// import colors from "../colors/colors";
-// const SCREEN_WIDTH = Dimensions.get("window").width;
-// const Home = ({ navigation }) => {
-//   return (
-//     // Top View
-//     <ScrollView
-//       style={{
-//         backgroundColor: "#000",
-//         flex: 1,
-//       }}
-//     >
-//       {/* Style Top View */}
-//       <View
-//         style={{
-//           backgroundColor: colors.blue,
-//           height: "20%",
-//           borderBottomLeftRadius: 20,
-//           borderBottomRightRadius: 20,
-//           paddingHorizontal: 20,
-//         }}
-//       >
-
->>>>>>> master
-        
         {/* Search */}
         <LinearGradient
           colors={["transparent", "transparent"]}
@@ -281,7 +248,6 @@ const device_type = ({ navigation }) => {
           </ScrollView>
         </View>
 
-<<<<<<< HEAD
         <View
           style={{
             height: 1,
@@ -292,19 +258,7 @@ const device_type = ({ navigation }) => {
             borderRadius: 30,
           }}
         ></View>
-=======
 
-//         <View
-//           style={{
-//             height: 1,
-//             backgroundColor: "#EA5455",
-//             width: 340,
-//             marginTop: 10,
-//             marginLeft: -15,
-//             borderRadius: 30,
-//           }}
-//         ></View>
->>>>>>> master
 
         {/* Cables/Wires*/}
         <View
